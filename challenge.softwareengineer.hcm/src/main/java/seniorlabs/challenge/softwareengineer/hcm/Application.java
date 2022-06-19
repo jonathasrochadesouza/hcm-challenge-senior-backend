461c3d89f1f22cc83539947f7cef33ebf8fda1fa
Jonathas Rocha <62067474+jonathasrochadesouza@users.noreply.github.com>
2022-06-19 17:18:42 -0300

    Iniciada a implementação da arquitetura orientada a eventos com o Apache Kafka, através do Docker

--- challenge.softwareengineer.hcm/src/main/java/seniorlabs/challenge/softwareengineer/hcm/Application.java
+++ challenge.softwareengineer.hcm/src/main/java/seniorlabs/challenge/softwareengineer/hcm/Application.java
@@ -1,18 +1,30 @@
 package seniorlabs.challenge.softwareengineer.hcm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import seniorlabs.challenge.softwareengineer.hcm.producer.AplicacaoProducer;
+import seniorlabs.challenge.softwareengineer.hcm.producer.events.ProdutorEvento;
 
 /**
- *
+ * Aplicação, classe que permite rodar a aplicação
  *
  * @author Jonathas Rocha
  */
+@Slf4j
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		Application aplicacao = new Application();
+		aplicacao.iniciarProducer();
+	}
+
+	private void iniciarProducer() {
+		log.info("Iniciando a aplicação");
+		ProdutorEvento produtor = new ProdutorEvento();
+		produtor.executar();
 	}
 
 }
