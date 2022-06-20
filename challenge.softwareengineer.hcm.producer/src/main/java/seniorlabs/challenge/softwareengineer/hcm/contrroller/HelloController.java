6dc226f0ad52601d8b3f464001e1555e7a5bff12
Jonathas Rocha <62067474+jonathasrochadesouza@users.noreply.github.com>
2022-06-20 18:35:27 -0300

    🛠 Realizado alguns ajustes

--- challenge.softwareengineer.hcm.producer/src/main/java/seniorlabs/challenge/softwareengineer/hcm/contrroller/HelloController.java
+++
@@ -1,9 +0,0 @@
-package seniorlabs.challenge.softwareengineer.hcm.contrroller;
-
-public class HelloController {
-
-    public String hello(String name) {
-        return String.format("Hello, %s", name);
-    }
-
-}
